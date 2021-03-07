b75d8dcb2ba4e7bf5facc4149381aeeab0fe372c
SadisticJedi <SadisticJedi@gmail.com>
2021-03-07 18:46:35 +0100

    added experimental agent

---
+++ bomberman_rosa_diaz/fallbacks.py
@@ -0,0 +1,23 @@
+import contextlib
+
+
+class QuietFallback:
+    def __getattr__(self, item):
+        return self
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+
+try:
+    with contextlib.redirect_stdout(None):
+        import pygame
+        LOADED_PYGAME = True
+except ModuleNotFoundError:
+    pygame = QuietFallback()
+    LOADED_PYGAME = True
+
+try:
+    from tqdm import tqdm
+except ModuleNotFoundError:
+    tqdm = lambda iterable, *args, **kwargs: iterable
