fd882ed8e7d4def3d113c289a0793d308c5b1908
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-03-13 12:34:25 +0100

    Remove useless code

--- agent_code/terry_jeffords/callbacks.py
+++ agent_code/terry_jeffords/callbacks.py
@@ -1,16 +1,13 @@
 import os
 import pickle
 import random
-from collections import deque
 
 import numpy as np
 
-import torch
 
-from agent_code.terry_jeffords.model import DQN
+import settings
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
-
 
 def setup(self):
     """
@@ -30,7 +27,6 @@ def setup(self):
         self.logger.info("Setting up model from scratch.")
         weights = np.random.rand(len(ACTIONS))
         self.model = weights / weights.sum()
-        #self.test_model = DQN(1734, 6)
     else:
         self.logger.info("Loading model from saved state.")
         with open("terry-jeffords-model.pt", "rb") as file:
@@ -49,7 +45,7 @@ def act(self, game_state: dict) -> str:
     # todo Exploration vs exploitation
     self.logger.info('Picking action according to rule set')
 
-    features = state_to_features(game_state)
+    features = state_to_features_hybrid(game_state)
 
     random_prob = .1
     if self.train and random.random() < random_prob:
@@ -60,68 +56,8 @@ def act(self, game_state: dict) -> str:
     self.logger.debug("Querying model for action.")
 
     # TODO Call get action
-    #return np.random.choice(ACTIONS, p=self.model)
-    return allowed_actions[outindex]
+    return np.random.choice(ACTIONS, p=self.model)
 
-
-
-import os
-import pickle
-import random
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
-import settings
-
-import numpy as np
-
-
-ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
-
-def setup(self):
-    """
-    Setup your code. This is called once when loading each agent.
-    Make sure that you prepare everything such that act(...) can be called.
-    When in training mode, the separate `setup_training` in train.py is called
-    after this method. This separation allows you to share your trained agent
-    with other students, without revealing your training code.
-    In this example, our model is a set of probabilities over actions
-    that are is independent of the game state.
-    :param self: This object is passed to all callbacks and you can set arbitrary values.
-    """
-    if self.train or not os.path.isfile("my-saved-model.pt"):
-        self.logger.info("Setting up model from scratch.")
-        weights = np.random.rand(len(ACTIONS))
-        self.model = weights / weights.sum()
-    else:
-        self.logger.info("Loading model from saved state.")
-        with open("my-saved-model.pt", "rb") as file:
-            self.model = pickle.load(file)
-
-
-def act(self, game_state: dict) -> str:
-    """
-    Your agent should parse the input, think, and take a decision.
-    When not in training mode, the maximum execution time for this method is 0.5s.
-    :param self: The same object that is passed to all of your callbacks.
-    :param game_state: The dictionary that describes everything on the board.
-    :return: The action to take as a string.
-    """
-    # todo Exploration vs exploitation
-
-    random_prob = .1
-    if self.train and random.random() < random_prob:
-        self.logger.debug("Choosing action purely at random.")
-        # 80%: walk in any direction. 10% wait. 10% bomb.
-        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
-
-    self.logger.debug("Querying model for action.")
-    #return np.random.choice(ACTIONS, p=self.model)
-    # just let a peaceful agent run for getting game states
-    return np.random.choice(ACTIONS[:-1], p=[.3, .2, .2, .2, .1])
 
 def state_to_features_hybrid(game_state: dict) -> np.array:
     """
