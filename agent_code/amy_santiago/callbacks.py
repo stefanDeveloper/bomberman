5c84dd53d7e67ccc6262ee573f74f5599ae3e5c2
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-03-14 17:11:08 +0100

    First draft of DQN

---
+++ agent_code/amy_santiago/callbacks.py
@@ -0,0 +1,117 @@
+import math
+import os
+import pickle
+import random
+
+import numpy as np
+import torch
+
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    if self.train or not os.path.isfile("my-saved-model.pt"):
+        self.logger.info("Setting up model from scratch.")
+    else:
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    # todo Exploration vs exploitation
+    #random_prob = .1
+    #if self.train and random.random() < random_prob:
+      #  self.logger.debug("Choosing action purely at random.")
+        # 80%: walk in any direction. 10% wait. 10% bomb.
+     #   return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+    #self.logger.debug("Querying model for action.")
+    #return np.random.choice(ACTIONS, p=self.model)
+
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * game_state["step"] / EPS_DECAY)
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            features = state_to_features(game_state)
+            features_tensor = torch.from_numpy(features).float()
+            action = self.policy_net(features_tensor)
+            print(ACTIONS[torch.argmax(action)])
+            return ACTIONS[torch.argmax(action)]
+    else:
+        return torch.tensor([[random.randrange(len(ACTIONS))]], dtype=torch.long)
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+
+    # TODO Turn map to have same view of user, good idea?
+
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    field_shape = game_state["field"].shape
+
+    # Create Hybrid Matrix with field shape x vector of size 5 to encode field state
+    hybrid_matrix = np.zeros(field_shape + (5,), dtype=np.double)
+
+    # others
+    for i in game_state["others"]:
+        hybrid_matrix[i[3], 0] = 1
+
+    # bombs
+    for i in game_state["bombs"]:
+        hybrid_matrix[i[0], 1] = 1
+
+    # coins
+    for i in game_state["coins"]:
+        hybrid_matrix[i, 2] = 1
+
+    # crates
+    hybrid_matrix[:, :, 3] = np.where(game_state["field"] == 1, 1, 0)
+
+    # walls
+    hybrid_matrix[:, :, 4] = np.where(game_state["field"] == -1, 1, 0)
+
+    final_vector = np.append(hybrid_matrix.reshape(-1), (game_state["self"][3]))
+
+    return final_vector
---
+++ agent_code/amy_santiago/train.py
@@ -0,0 +1,187 @@
+import pickle
+import random
+import numpy as np
+from collections import namedtuple, deque
+from typing import List
+
+import torch
+from torch import optim
+import torch.nn.functional as F
+
+import events as e
+
+# This is only an example!
+from .callbacks import state_to_features, ACTIONS
+from .model import DQN
+from .replay_memory import ReplayMemory
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# Hyper parameters -- DO modify
+TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+# Events
+PLACEHOLDER_EVENT = "PLACEHOLDER"
+
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # Example: Setup an array that will note transition tuples
+    # (s, a, r, s')
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+    n_actions = 6
+
+    self.policy_net = DQN(1447, n_actions)
+    self.target_net = DQN(1447, n_actions)
+    self.target_net.load_state_dict(self.policy_net.state_dict())
+    self.target_net.eval()
+
+    self.optimizer = optim.RMSprop(self.policy_net.parameters())
+    self.memory = ReplayMemory(10000)
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    # Idea: Add your own events to hand out rewards
+    if ...:
+        events.append(PLACEHOLDER_EVENT)
+
+    self.memory.push(state_to_features(old_game_state), self_action, state_to_features(new_game_state),
+                     reward_from_events(self, events))
+
+    # state_to_features is defined in callbacks.py
+    self.transitions.append(
+        Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state),
+                   reward_from_events(self, events)))
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+
+    This is similar to reward_update. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+    self.transitions.append(
+        Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
+
+    # Store the model
+    with open("my-saved-model.pt", "wb") as file:
+        pickle.dump(self.policy_net, file)
+
+    optimize_model(self)
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Here you can modify the rewards your agent get so as to en/discourage
+    certain behavior.
+    """
+    game_rewards = {
+        e.COIN_COLLECTED: 1,
+        e.KILLED_OPPONENT: 5,
+        PLACEHOLDER_EVENT: -.1  # idea: the custom event is bad
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
+
+
+def optimize_model(self):
+    if len(self.memory) < BATCH_SIZE:
+        return
+
+    transitions = self.memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), dtype=torch.bool)
+    print(batch.state)
+    training_states = torch.tensor(batch.state[1:]).float()
+    training_next_states = torch.tensor(batch.next_state[1:]).float()
+    training_rewards = torch.tensor(batch.reward[1:]).float()
+    training_actions = batch.reward[1:]
+
+    non_final_next_states = torch.cat([s for s in training_next_states
+                                       if s is not None])
+
+    action_indices = torch.zeros((training_states.shape[0], len(ACTIONS))).bool()
+
+    for i in range(len(training_actions)):
+        action_indices[i][ACTIONS.index(training_actions[i])] = True
+
+    state_batch = torch.cat(training_states)
+    action_batch = torch.cat(training_actions)
+    reward_batch = torch.cat(training_rewards)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE)
+    next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    self.optimizer.zero_grad()
+    loss.backward()
+    for param in self.policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    self.optimizer.step()
