f87697615a95249a438306aba81dd1e1d981ce96
SadisticJedi <SadisticJedi@gmail.com>
2021-03-15 19:00:33 +0100

    Added logging functionality

--- agent_code/rosa_diaz/loss_plotter.py
+++ agent_code/rosa_diaz/loss_plotter.py
@@ -11,5 +11,16 @@ def plot_loss(loss_list):
     plt.ylabel("loss")
     plt.show()
 
+def plot_reward(reward_list):
+    t = np.arange(len(reward_list))
+    l = np.array(reward_list)
+    plt.plot(t, l)
+    # plt.yscale("log")
+    plt.xlabel("step")
+    plt.ylabel("reward")
+    plt.show()
+
 loss = np.loadtxt("loss_log.txt")
+reward = np.loadtxt("reward_log.txt")
 plot_loss(loss)
+plot_reward(reward)
--- agent_code/rosa_diaz/train.py
+++ agent_code/rosa_diaz/train.py
@@ -22,7 +22,7 @@ Transition = namedtuple('Transition',
 # Hyper parameters -- DO modify
 TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
-BATCH_SIZE = 10 # default = 127
+BATCH_SIZE = 127 # default = 127
 GAMMA = 0.999
 EPS_START = 0.9
 EPS_END = 0.05
@@ -248,7 +248,7 @@ def optimize_model(self):
     reward_batch = torch.tensor(batch.reward).float()
 
     with open("reward_log.txt", "a") as reward_log:
-        reward_log.write(str(torch.mean(reward_batch)) + "\t")
+        reward_log.write(str(torch.mean(reward_batch).item()) + "\t")
 
     action_batch = torch.zeros((state_batch.shape[0], len(ACTIONS)), dtype=torch.int64)
 
