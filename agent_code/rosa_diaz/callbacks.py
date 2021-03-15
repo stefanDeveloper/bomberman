018f70df96a305d1093f310d8a443b0fcc71cb0f
SadisticJedi <SadisticJedi@gmail.com>
2021-03-15 16:41:58 +0100

    got newest version from amy_santiago

--- agent_code/rosa_diaz/callbacks.py
+++ agent_code/rosa_diaz/callbacks.py
@@ -9,24 +9,26 @@ import torch
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
+steps_done = 0
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
 
 def setup(self):
+    global steps_done
     """
     Setup your code. This is called once when loading each agent.
     Make sure that you prepare everything such that act(...) can be called.
-
     When in training mode, the separate `setup_training` in train.py is called
     after this method. This separation allows you to share your trained agent
     with other students, without revealing your training code.
-
     In this example, our model is a set of probabilities over actions
     that are is independent of the game state.
-
     :param self: This object is passed to all callbacks and you can set arbitrary values.
     """
+
+    steps_done = 0
+
     if self.train or not os.path.isfile("my-saved-model.pt"):
         self.logger.info("Setting up model from scratch.")
     else:
@@ -36,27 +38,19 @@ def setup(self):
 
 
 def act(self, game_state: dict) -> str:
+    global steps_done
     """
     Your agent should parse the input, think, and take a decision.
     When not in training mode, the maximum execution time for this method is 0.5s.
-
     :param self: The same object that is passed to all of your callbacks.
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
     # todo Exploration vs exploitation
-    #random_prob = .1
-    #if self.train and random.random() < random_prob:
-      #  self.logger.debug("Choosing action purely at random.")
-        # 80%: walk in any direction. 10% wait. 10% bomb.
-     #   return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
-
-    #self.logger.debug("Querying model for action.")
-    #return np.random.choice(ACTIONS, p=self.model)
-
     sample = random.random()
     eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * game_state["step"] / EPS_DECAY)
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
             # t.max(1) will return largest column value of each row.
@@ -91,25 +85,25 @@ def state_to_features(game_state: dict) -> np.array:
     field_shape = game_state["field"].shape
 
     # Create Hybrid Matrix with field shape x vector of size 5 to encode field state
-    hybrid_matrix = np.zeros(field_shape + (1,), dtype=np.double)
+    hybrid_matrix = np.zeros(field_shape + (5,), dtype=np.double)
 
     # others
-    #for i in game_state["others"]:
-    #    hybrid_matrix[i[3], 0] = 1
+    for i in game_state["others"]:
+        hybrid_matrix[i[3], 0] = 1
 
     # bombs
-    #for i in game_state["bombs"]:
-    #    hybrid_matrix[i[0], 1] = 1
+    for i in game_state["bombs"]:
+        hybrid_matrix[i[0], 1] = 1
 
     # coins
     for i in game_state["coins"]:
-        hybrid_matrix[i, 1] = 1
+        hybrid_matrix[i, 2] = 1
 
     # crates
-    #hybrid_matrix[:, :, 3] = np.where(game_state["field"] == 1, 1, 0)
+    hybrid_matrix[:, :, 3] = np.where(game_state["field"] == 1, 1, 0)
 
     # walls
-    #hybrid_matrix[:, :, 4] = np.where(game_state["field"] == -1, 1, 0)
+    hybrid_matrix[:, :, 4] = np.where(game_state["field"] == -1, 1, 0)
     final_vector = np.append(hybrid_matrix.reshape(-1), (game_state["self"][3]))
-    #print(final_vector)
+
     return final_vector
